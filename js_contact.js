695c89228b0c89df8c68e4e3a031754c50e520b1
Endi_Trico <enditrico@gmail.com>
2022-05-21 14:45:23 +0200

    Contact is done

---
+++ js_contact.js
@@ -0,0 +1,10 @@
+function typeWriter1() {
+    var x = document.getElementById("location1");
+    x.style.visibility = "visible";
+}
+
+function typeWriter2() {
+    var x = document.getElementById("location2");
+    x.style.visibility = "visible";
+}
+
