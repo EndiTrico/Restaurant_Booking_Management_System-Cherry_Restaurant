bc48da01be7fde21f24f66b9eee593141e77b024
Endi_Trico <enditrico@gmail.com>
2022-06-03 20:02:46 +0200

    required

--- registration.php
+++ registration.php
@@ -45,6 +45,7 @@ if (isset($_POST['submit'])) //if submit button is pressed)
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="./images/restaurant_logoo.png">
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
