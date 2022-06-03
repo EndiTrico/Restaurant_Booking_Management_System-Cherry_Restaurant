e5153b76fdc98ebf28334cbb237905f3b6a14269
Dajna Nako <danjanako9@gmail.com>
2022-06-03 21:51:10 +0200

    hshh

--- user_reservation.php
+++ user_reservation.php
@@ -13,7 +13,7 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-    <title>Cherry Restaurant | Gallery</title>
+    <title>Cherry Restaurant | Reservation</title>
 </head>
 
 <body>
@@ -23,45 +23,45 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
+                <h1 class="page_title">Fill up the form <br>to book your table now!</h1>
                     <div class="form-group">
-                        <label>First Name</label>
+                        <label class="fname_style">First Name</label>
                         <input type="text" class="form-control" name="first_name" placeholder="First Name" required="required">
                     </div>
                     <div class="form-group">
-                        <label>Last Name</label>
+                        <label class="info_style">Last Name</label>
                         <input type="text" class="form-control" name="lname" placeholder="Last Name" required="required">
                     </div>
                     <div class="form-group">
-                        <label>Enter Date</label>
+                        <label class="info_style">Enter Date</label>
                         <input type="date" class="form-control" name="date" placeholder="Date" required="required">
                     </div>
                    
                     <div class="form-group">
-                        <label>Enter number of Guests</label>
+                        <label class="info_style">Enter number of Guests</label>
                         <input type="number" class="form-control" min="1" name="num_guests" placeholder="Guests" required="required">
-                        <small class="form-text text-muted">Minimum value is 1</small>
+                        <small class="form-text text-muted">*Minimum value is 1</small>
                     </div>
                     <div class="form-group">
-                        <label for="guests">Enter your Telephone Number</label>
+                        <label for="guests" class="info_style">Enter your Telephone Number</label>
                         <input type="telephone" class="form-control" name="tele" placeholder="Telephone" required="required">
-                        <small class="form-text text-muted">Telephone must be 6-20 characters long</small>
+                        <small class="form-text text-muted">*Telephone must be 6-20 characters long</small>
                     </div>
                     <div class="form-group">
-                        <label>Enter extra Comments</label>
+                        <label class="info_style">Enter extra Comments</label>
                         <textarea class="form-control" name="comments" placeholder="Comments" rows="3"></textarea>
                     </div>
                     <div class="form-group">
                         <label class="checkbox-inline"><input type="checkbox" required="required"> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
                     </div>
                     <div class="form-group">
-                        <button type="submit" name="reserv-submit" class="btn btn-dark btn-lg btn-block">Submit Reservation</button>
+                        <button type="submit" name="reserv-submit" class="btn btn-dark btn-lg btn-block button_style">Submit Reservation</button>
                     </div>
                     </form>
                     <br><br>
                 </div>
             </div>
         </div>
-    </div>
     </div>
     <?php include "footer.php"; ?>
 
