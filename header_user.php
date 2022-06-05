0a6ca2a2ca9e2ff070c7a0ea9b5dcf7cc9564c46
Dajna Nako <danjanako9@gmail.com>
2022-06-05 21:11:51 +0200

    ahhhs

--- header_user.php
+++ header_user.php
@@ -7,10 +7,8 @@
         <li><a href="user_menu.php">Menu</a></li>
         <li><a href="user_gallery.php">Gallery</a></li>
         <li><a href="user_contact.php">Contact Us</a></li>
-        <li><a href="user_reservation.php">Reservation</a></li>
-        <li><a href="user_view_reservation.php">Show Reservation</a></li>
-        <li><a href="user_profile.php">Profile</a></li>
-        <li><a href="logout.php">Log Out</a></li>
+        <li><a href="reservation.php">Reservation</a></li>
+        <li><a href="settings.php">Settings</a></li>
     </ul>
     
 </nav>
---
+++ reservation.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/png" href="./images/restaurant_logoo.png">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="style_options.css">
+
+	<title>Cherry Restaurant | Reservation</title>
+</head>
+
+<body>
+	<?php include "header_user.php"; ?>
+
+
+	<div class="container1">
+		<div id="loginFormContainer">
+				<p class="login-text" style="font-size: 2rem; font-weight: 800;">Reservation</p>
+				<div>
+					<label style="font-weight:bold;">What do you want to do?</label>
+				</div>
+                <h3 class="reservation"><a href="user_reservation.php">Book a Table</a></h3>
+                <h3 class="reservation"><a href="user_view_reservation.php">Show My Reservations</a></h3>
+</body>
+
+</html>
