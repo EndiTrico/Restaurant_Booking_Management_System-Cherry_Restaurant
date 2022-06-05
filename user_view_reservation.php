ea616620465064a4513c173dbaae20d9fcc2bd36
Endi_Trico <enditrico@gmail.com>
2022-06-05 19:22:49 +0200

    nnn

--- user_view_reservation.php
+++ user_view_reservation.php
@@ -1,3 +1,30 @@
+<?php
+session_start();
+
+require 'database/config.php';
+require 'database/opendb.php';
+
+if (isset($_GET['Reservation_ID'])) {
+    $id = $_GET['Reservation_ID'];
+
+    $today = date("yy-m-d");
+    $date = "SELECT * FROM reservation Where Reservation_ID = " . $id . " AND DATE > CURRENT_TIMESTAMP";
+
+    $result1 = mysqli_query($conn, $date);
+
+    $numrows = mysqli_num_rows($result1);
+
+    if ($numrows > 0) {
+        $delete = mysqli_query($conn, "Delete from reservation where Reservation_ID = " . $id);
+    } else {
+        echo '<script type="text/javascript">';
+        echo 'alert("You cannot delete previous reservation")';
+        echo '</script>';
+    }
+}
+?>
+
+
 <!DOCTYPE html>
 <html>
 
@@ -45,7 +72,7 @@
     $result1 = mysqli_query($conn, $userid);
     $row2 = mysqli_fetch_array($result1);
 
-    $sql = "SELECT * FROM reservation WHERE User_ID = $row2[0]";
+    $sql = "SELECT * FROM reservation WHERE User_ID = $row2[0] ORDER BY Date desc";
 
     $result = mysqli_query($conn, $sql);
 
@@ -68,19 +95,19 @@
                         <th class="table-danger" scope="col"></th>
                     </tr>
                 </thead> <tbody>';
-        while ($row = mysqli_fetch_array($result)) {
+        while ($row = mysqli_fetch_assoc($result)) {
             echo "
                     <tr>
-                    <input name='reserv_id' type='hidden' value=" . $row[0] . ">
-                    <td>" . $row[1] . "</td>
-                    <td>" . $row[2] . "</td>
-                    <td>" . $row[3] . "</td>
-                    <td>" . $row[4] . "</td>
-                    <td>" . $row[5] . "</td>
-                    <td>" . $row[6] . "</td>
-                    <td>" . $row[7] . "</td>
-                    <td><textarea readonly>" . $row[8] . "</textarea></td>
-                    <td class='table-danger'><button type='submit' name='delete-submit' class='btn btn-danger btn-sm' id='cancel reservation'>Cancel</button></td>
+                    <input name='reserv_id' type='hidden' value=" . $row['Reservation_ID'] . ">
+                    <td>" . $row['First_Name'] . "</td>
+                    <td>" . $row['Last_Name'] . "</td>
+                    <td>" . $row['Date'] . "</td>
+                    <td>" . $row['Time'] . "</td>
+                    <td>" . $row['Number_of_Guests'] . "</td>
+                    <td>" . $row['Location'] . "</td>
+                    <td>" . $row['Telephone_Number'] . "</td>
+                    <td><textarea readonly>" . $row['Comments'] . "</textarea></td>
+                    <td class='table-danger'><a class= 'btn btn-danger btn-sm' href='user_view_reservation.php?Reservation_ID= " . $row['Reservation_ID'] . "'>Cancel</button></td>
                     </tr>
               ";
         }
