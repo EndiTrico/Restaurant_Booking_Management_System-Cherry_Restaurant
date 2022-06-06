a2e6dbe77815c8aefbb39bcd65c4486dda64c6ac
Endi_Trico <enditrico@gmail.com>
2022-06-06 23:52:32 +0200

    Update reservation.php

--- reservation.php
+++ reservation.php
@@ -28,10 +28,10 @@
 				<br>
 				<p style="font-weight:bold; text-align: center; ">What do you want to do?</p><br>
 			</div>
-			<h4 class="reservation" style="text-align: center;"><a id="booking" href="user_reservation.php" style=" color: white; margin-top: 5px;
+			<h4 class="reservation" style="text-align: center;"><a id= "booking" href="user_reservation.php" style="padding-left: 90px !important; padding-right: 90px !important; color: white; margin-top: 5px;
     padding: 10px 10px; background-color: brown !important; text-decoration: none; border-radius: 50px" onmouseover="myfunctionover()" onmouseout="myfunctionout()">Book a Table</a></h4><br><br>
-			<h4 class="reservation" style="text-align: center;"><a id="viewReservation" href="user_view_reservation.php" style="color: white; margin-top: 5px;
-    padding: 10px 10px; background-color: brown !important; text-decoration: none; border-radius: 50px" onmouseover="myfunctionover1()" onmouseout="myfunctionout1()">Show Reservations</a></h4>
+			<h4 class="reservation" style="text-align: center;"><a id="viewReservation" href="user_view_reservation.php" style="padding-left: 30px !important; padding-right: 30px !important; color: white; margin-top: 5px;
+    padding: 10px 10px; background-color: brown !important; text-decoration: none; border-radius: 50px" onmouseover="myfunctionover1()" onmouseout="myfunctionout1()">Show My Reservations</a></h4>
 		</div>
 	</div>
 
@@ -51,6 +51,8 @@
 		function myfunctionout1() {
 			document.getElementById("viewReservation").style.backgroundColor = "brown";
 		}
+
+
 	</script>
 
 </body>
