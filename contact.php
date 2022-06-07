ef6f4de7d59137bfef3bf3d83f411fba4a08614c
Endi_Trico <enditrico@gmail.com>
2022-06-07 18:01:28 +0200

    CTRL + F5

--- contact.php
+++ contact.php
@@ -2,7 +2,6 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="style_contact.css">
     <link rel="icon" type="image/png" href="./images/restaurant_logoo.png">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
 
@@ -10,7 +9,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
+    <link rel="stylesheet" href="style_contact.css">
     <script src="js_contact.js">
 
     </script>
@@ -22,8 +21,8 @@
     <?php include "header.php"; ?>
 
     <div>
-        <img class="main_photo" style="margin-top: 80px;" src="./Images/Images_Contact/photo1.png" alt="Photo Contact">
-        <img class="main_burgerphoto" style="	margin-top: -328px;" src="./Images/Images_Contact/photo2.png" alt="Burger">
+        <img class="main_photo" src="./Images/Images_Contact/photo1.png" alt="Photo Contact">
+        <img class="main_burgerphoto" src="./Images/Images_Contact/photo2.png" alt="Burger">
         <h1 class="contactus_text">Contact Us</h1>
     </div>
 
--- user_gallery.php
+++ user_gallery.php
@@ -1,7 +1,6 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="style_gallery.css">
     <link rel="icon" type="image/png" href="./images/restaurant_logoo.png">
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
@@ -10,6 +9,7 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
+    <link rel="stylesheet" href="style_gallery.css">
     <title>Cherry Restaurant | Gallery</title>
 </head>
 
