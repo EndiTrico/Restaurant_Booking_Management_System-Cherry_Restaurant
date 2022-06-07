7aa06946062911bd63c5ac5d353b556720540e36
Endi_Trico <enditrico@gmail.com>
2022-06-07 17:52:15 +0200

    nnn

--- reservation.php
+++ reservation.php
@@ -28,9 +28,9 @@
 				<br>
 				<p style="font-weight:bold; text-align: center; ">What do you want to do?</p><br>
 			</div>
-			<h4 class="reservation" style="text-align: center;"><a id= "booking" href="user_reservation.php" style="padding-left: 90px !important; padding-right: 90px !important; color: white; margin-top: 5px;
+			<h4  style="text-align: center;"><a id= "booking" href="user_reservation.php" style="padding-left: 90px !important; padding-right: 90px !important; color: white; margin-top: 5px;
     padding: 10px 10px; background-color: brown !important; text-decoration: none; border-radius: 50px" onmouseover="myfunctionover()" onmouseout="myfunctionout()">Book a Table</a></h4><br><br>
-			<h4 class="reservation" style="text-align: center;"><a id="viewReservation" href="user_view_reservation.php" style="padding-left: 30px !important; padding-right: 30px !important; color: white; margin-top: 5px;
+			<h4  style="text-align: center;"><a id="viewReservation" href="user_view_reservation.php" style="padding-left: 30px !important; padding-right: 30px !important; color: white; margin-top: 5px;
     padding: 10px 10px; background-color: brown !important; text-decoration: none; border-radius: 50px" onmouseover="myfunctionover1()" onmouseout="myfunctionout1()">Show My Reservations</a></h4>
 		</div>
 	</div>
