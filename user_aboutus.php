8a670710a612b87d8f7e156419d5164fadfd58c7
Dajna Nako <danjanako9@gmail.com>
2022-06-07 19:36:20 +0200

    xjhcjach

--- user_aboutus.php
+++ user_aboutus.php
@@ -4,6 +4,7 @@
     <link rel="stylesheet" href="style_aboutus.css">
     <link rel="icon" type="image/png" href="./images/restaurant_logoo.png">
 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
